b00437b785d7c06138cc78ed1b7c706c85aefea3
Tim Rayburn <tim@timrayburn.net>
2012-04-16 17:25:52 -0500

    Self-Hosting

---
+++ StockFinder/StockLookup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace StockFinder
+{
+    // Contract
+    [ServiceContract]
+    public interface IStockLookup
+    {
+        [OperationContract]
+    	double GetPrice(string symbol);
+        [OperationContract]
+        StockInfo GetInfo(string symbol);
+    }
+
+    // DataContract
+    [DataContract]
+    public class StockInfo
+    {
+        [DataMember]
+        public string Symbol { get; set; }
+        [DataMember]
+        public double Price { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+    }
+
+    // Service Implementation
+    public class StockLookup : IStockLookup
+    {
+        public double GetPrice(string symbol)
+        {
+            if (symbol.Equals("MSFT", StringComparison.InvariantCultureIgnoreCase))
+                return 598.27;
+            else return 123.45;
+        }
+
+        public StockInfo GetInfo(string symbol)
+        {
+            if (symbol.Equals("MSFT", StringComparison.InvariantCultureIgnoreCase))
+                return new StockInfo
+                {
+                    Symbol = "MSFT",
+                    Price = 598.27,
+                    Description = "Creators of the amazing .NET Framework"
+                };
+            else return new StockInfo
+            {
+                Symbol = symbol,
+                Price = 123.45,
+                Description = "Some random stock"
+            };
+        }
+    }
+}
---
+++ WcfTopics-Basic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using StockFinder;
+
+namespace WcfTopics_Basic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(StockLookup)))
+            {
+                host.Open();
+                Console.WriteLine("Host is now running...");
+                Console.WriteLine(">>> Press Enter To Stop <<<");
+                Console.ReadLine();
+                host.Close();
+            }
+        }
+    }
+}
