10683a033a8933929405dd79ec1460bc2743a2aa
Tim Rayburn <tim@timrayburn.net>
2012-04-16 17:42:44 -0500

    CodeConfigure with HttpGet enabled

--- WCFTopcics-CodeConfigure/Program.cs
+++ WCFTopcics-CodeConfigure/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.ServiceModel;
 using StockFinder;
+using System.ServiceModel.Description;
 
 namespace WCFTopcics_CodeConfigure
 {
@@ -13,13 +14,19 @@ namespace WCFTopcics_CodeConfigure
         {
             // This is a code-configured host, NOTE that
             // the project has no App.Config
-            using (var host = new ServiceHost(typeof(StockLookup)))
+            using (var host = new ServiceHost(
+                typeof(StockLookup), 
+                new Uri("http://localhost:1701/")))
             {
                 var binding = new WSHttpBinding();
-                host.AddServiceEndpoint(
-                    typeof(IStockLookup),               // Contract
-                    binding,                            // Binding
-                    "http://localhost:1701/Stock");     // Address
+                var ep = host.AddServiceEndpoint(
+                    typeof(IStockLookup),   // Contract
+                    binding,                // Binding
+                    "Stock");               // Address
+
+                //var smb = new ServiceMetadataBehavior();
+                //smb.HttpGetEnabled = true;
+                //host.Description.Behaviors.Add(smb);
 
                 host.Open();
                 Console.WriteLine("Host is now running...");
